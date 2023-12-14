9e3c758d2fa56f34e31061ad9bb4f79cbde08194
Collin Duncan <3679940+cgduncan7@users.noreply.github.com>
2023-12-14 12:02:47 +0100

    Day 12 part1 and day 13 part 1/2

--- day12/solutions/erl/day12.erl
+++ day12/solutions/erl/day12.erl
@@ -1,11 +1,109 @@
--module(dayXX).
+-module(day12).
 -import(harness, [run/5]).
 -export([run/1]).
 
 run(Filename) ->
-  harness:run("", Filename, fun solve/2, unknown, bytes).
+  harness:run("Part 1", Filename, fun solve_pt1/2, 0, lines).
+  % harness:run("Part 2", Filename, fun solve_pt2/2, 0, lines).
 
-solve(Data, State) ->
+solve_pt1(Data, State) ->
   case Data of
-    _ -> todo
+    nil -> State;
+    D -> {Arr, Dmg} = parse_input(D), State + erlang:length(get_valid_expansions(expand("", unknown, Arr, Dmg), Dmg))
   end.
+
+solve_pt2(Data, State) ->
+  case Data of
+    nil -> State;
+    D ->
+      {Arr, Dmg} = parse_input_with_unfolding(D),
+      A = erlang:length(get_valid_expansions(expand("", unknown, Arr, Dmg), Dmg)),
+      io:fwrite("~s ~w => ~w~n", [Arr, Dmg, A]),
+      State + A
+  end.
+
+parse_input(Input) ->
+  [Arrangement|DamagedSprings] = string:lexemes(Input, " ,"),
+  {Arrangement, lists:nthtail(1, lists:foldl(fun (I, A) -> {V, _} = string:to_integer(I),  A ++ [0, V] end, [], DamagedSprings))}.
+
+parse_input_with_unfolding(Input) ->
+  [Arrangement|DamagedSprings] = string:lexemes(Input, " ,"),
+  ExpandedArrangement = lists:flatten(lists:join("?", lists:duplicate(5, Arrangement))),
+  {ExpandedArrangement, lists:foldl(
+    fun (E, Acc) ->
+      if
+        erlang:length(Acc) =:= 0 -> [E];
+        true -> Acc ++ [0] ++ [E]
+      end
+    end,
+    [],
+    lists:flatten(lists:duplicate(5, lists:map(fun (I) -> {V, _} = string:to_integer(I), V end, DamagedSprings)))
+  )}.
+
+get_damaged_groups(Arrangement) -> string:lexemes(Arrangement, ".").
+
+validate(Arrangement, DamagedSprings) ->
+  lists:all(
+    fun ({A,B}) -> erlang:length(A) =:= B end,
+    lists:zip(
+      get_damaged_groups(Arrangement),
+      lists:filter(fun (S) -> S =/= 0 end, DamagedSprings),
+      {pad, {[], -1}}
+    )
+  ).
+
+validate_partial(Arrangement, DamagedSprings) ->
+  (erlang:length(Arrangement) >= lists:sum(DamagedSprings) + (erlang:length(DamagedSprings) div 2)) and
+  lists:all(
+    fun ({A,B}) -> erlang:length(A) =< B end,
+    lists:zip(
+      get_damaged_groups(lists:takewhile(fun (E) -> E =/= $? end, Arrangement)),
+      lists:filter(fun (S) -> S =/= 0 end, DamagedSprings),
+      trim
+    )
+  ).
+
+v_expand(C, G, A, D) ->
+  % io:fwrite("Current: ~s~n[~w] Arrangement: ~w Damaged: ~w~n~n", [C,G,A,D]),
+  expand(C,G,A,D).
+
+expand(Current, _, [], []) ->
+  {valid, Current};
+expand(Current, _, [$.|RestArrangement], [0|RestDamagedSprings]) ->
+  v_expand(Current ++ ".", out, RestArrangement, RestDamagedSprings);
+expand(Current, _, [$.|RestArrangement], DamagedSprings) ->
+  v_expand(Current ++ ".", out, RestArrangement, DamagedSprings);
+expand(_, _, [$#|_], [0|_]) -> invalid;
+expand(Current, _, [$#|RestArrangement], [1|RestDamagedSprings]) ->
+  v_expand(Current ++ "#", in, RestArrangement, RestDamagedSprings);
+expand(Current, _, [$#|RestArrangement], [HeadDamagedSprings|RestDamagedSprings]) ->
+  v_expand(Current ++ "#", in, RestArrangement, [HeadDamagedSprings-1|RestDamagedSprings]);
+expand(Current, _, [$?|RestArrangement], [0|RestDamagedSprings]) ->
+  v_expand(Current ++ ".", out, RestArrangement, RestDamagedSprings);
+expand(Current, _, [$?|RestArrangement], []) ->
+  v_expand(Current, out, [$.|RestArrangement], []);
+expand(Current, in, [$?|RestArrangement], DamagedSprings) ->
+  IsDamagedVariantValid = validate_partial([$#|RestArrangement], DamagedSprings),
+  if
+    IsDamagedVariantValid -> v_expand(Current, in, [$#|RestArrangement], DamagedSprings);
+    true -> invalid
+  end;
+expand(Current, _, [$?|RestArrangement], DamagedSprings) ->
+  IsWorkingVariantValid = validate_partial([$.|RestArrangement], DamagedSprings),
+  IsDamagedVariantValid = validate_partial([$#|RestArrangement], DamagedSprings),
+  if
+    IsWorkingVariantValid and IsDamagedVariantValid -> [v_expand(Current, out, [$.|RestArrangement], DamagedSprings), v_expand(Current, in, [$#|RestArrangement], DamagedSprings)];
+    not IsWorkingVariantValid and IsDamagedVariantValid -> v_expand(Current, in, [$#|RestArrangement], DamagedSprings);
+    IsWorkingVariantValid and not IsDamagedVariantValid -> v_expand(Current, out, [$.|RestArrangement], DamagedSprings);
+    not IsWorkingVariantValid and not IsDamagedVariantValid -> invalid
+  end;
+expand(_, _, _, _) -> invalid.
+
+get_valid_expansions(Expansions, DamagedSprings) ->
+  lists:filter(
+    fun (E) ->
+      case E of
+        invalid -> false;
+        {valid, A} -> validate(A, DamagedSprings)
+      end
+    end, lists:flatten([Expansions])).
--- day13/solutions/erl/day13.erl
+++ day13/solutions/erl/day13.erl
@@ -1,11 +1,108 @@
--module(dayXX).
+-module(day13).
 -import(harness, [run/5]).
 -export([run/1]).
 
-run(Filename) ->
-  harness:run("", Filename, fun solve/2, unknown, bytes).
+-record(rocks, {
+  rows=[],
+  cols=[]
+}).
 
-solve(Data, State) ->
+run(Filename) ->
+  harness:run("Part 1", Filename, fun solve_pt1/2, {#rocks{}, 0}, lines),
+  harness:run("Part 2", Filename, fun solve_pt2/2, {#rocks{}, 0}, lines).
+
+solve_pt1(Data, {#rocks{rows=Rows,cols=Cols}, Total}) ->
   case Data of
-    _ -> todo
+    "" -> {#rocks{}, Total+find_mirror(#rocks{rows=Rows,cols=rows_to_cols(Rows)})};
+    nil -> Total+find_mirror(#rocks{rows=Rows,cols=rows_to_cols(Rows)});
+    D -> {#rocks{rows=Rows ++ [D],cols=Cols}, Total}
   end.
+
+
+solve_pt2(Data, {#rocks{rows=Rows,cols=Cols}, Total}) ->
+  case Data of
+    "" -> {#rocks{}, Total+find_mirror(#rocks{rows=Rows,cols=rows_to_cols(Rows)}, smudge)};
+    nil -> Total+find_mirror(#rocks{rows=Rows,cols=rows_to_cols(Rows)}, smudge);
+    D -> {#rocks{rows=Rows ++ [D],cols=Cols}, Total}
+  end.
+
+rows_to_cols(Rows) ->
+  lists:foldl(
+    fun ({_, Col}, Ac) ->
+      Ac ++ [Col]
+    end,
+    [],
+    lists:sort(
+      maps:to_list(
+        lists:foldl(
+          fun ({Idx, Val}, A) ->
+            maps:put(Idx, lists:append([maps:get(Idx, A, []), [Val]]), A)
+          end,
+          maps:new(), 
+          lists:flatten(lists:map(fun lists:enumerate/1, Rows))
+        )
+      )
+    )
+  ).
+
+find_mirror(#rocks{rows=Rows,cols=Cols}) ->
+  PossibleRowMirrors = find_matching(Rows),
+  PossibleColMirrors = find_matching(Cols),
+  RowMirrors = lists:filter(fun ({Idx, _}) -> is_mirrored(Idx, Rows) end, PossibleRowMirrors),
+  ColMirrors = lists:filter(fun ({Idx, _}) -> is_mirrored(Idx, Cols) end, PossibleColMirrors),
+  if
+    erlang:length(RowMirrors) =:= 1 -> {Idx,_} = hd(RowMirrors), Idx * 100;
+    erlang:length(ColMirrors) =:= 1 -> {Idx,_} = hd(ColMirrors), Idx
+  end.
+
+find_mirror(#rocks{rows=Rows,cols=Cols}, smudge) ->
+  PossibleRowMirrors = find_matching(Rows, smudge),
+  PossibleColMirrors = find_matching(Cols, smudge),
+  RowMirrors = lists:filter(fun ({Idx, _}) -> is_mirrored(Idx, Rows, smudge) end, PossibleRowMirrors),
+  ColMirrors = lists:filter(fun ({Idx, _}) -> is_mirrored(Idx, Cols, smudge) end, PossibleColMirrors),
+  if
+    erlang:length(RowMirrors) =:= 1 -> {Idx,_} = hd(RowMirrors), Idx * 100;
+    erlang:length(ColMirrors) =:= 1 -> {Idx,_} = hd(ColMirrors), Idx
+  end.
+
+find_matching(RockRowsOrCols) ->
+  lists:filter(
+    fun ({Index, Elem}) ->
+      if
+        (Index+1) =< erlang:length(RockRowsOrCols) -> Elem =:= lists:nth(Index+1, RockRowsOrCols);
+        true -> false
+      end
+    end,
+    lists:enumerate(RockRowsOrCols)
+  ).
+
+is_mirrored(StartIndex, Rocks) ->
+  LeftIndices = lists:reverse(lists:seq(1, StartIndex)),
+  RightIndices = lists:seq(StartIndex+1, erlang:length(Rocks)),
+  MatchingIndices = lists:zip(LeftIndices, RightIndices, trim),
+  lists:all(fun ({L,R}) -> lists:nth(L, Rocks) =:= lists:nth(R, Rocks) end, MatchingIndices).
+
+is_mirrored(StartIndex, Rocks, smudge) ->
+  LeftIndices = lists:reverse(lists:seq(1, StartIndex)),
+  RightIndices = lists:seq(StartIndex+1, erlang:length(Rocks)),
+  MatchingIndices = lists:zip(LeftIndices, RightIndices, trim),
+  Differences = lists:foldl(
+    fun ({L,R}, Total) -> Total + find_differences(lists:nth(L, Rocks), lists:nth(R, Rocks)) end,
+    0,
+    MatchingIndices
+  ),
+  Differences =:= 1.
+
+find_matching(RockRowsOrCols, smudge) ->
+  lists:filter(
+    fun ({Index, Elem}) ->
+      if
+        (Index+1) =< erlang:length(RockRowsOrCols) -> find_differences(Elem, lists:nth(Index+1, RockRowsOrCols)) =< 1;
+        true -> false
+      end
+    end,
+    lists:enumerate(RockRowsOrCols)
+  ).
+
+find_differences(L, R) ->
+  erlang:length(lists:filter(fun ({LL, RR}) -> LL =/= RR end, lists:zip(L, R))).
